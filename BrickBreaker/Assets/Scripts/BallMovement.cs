b950787ffbab5eef42766f6b75aa919c5104188e
LuisDiegoFV <fer16344@uvg.edu.gt>
2017-04-02 17:39:31 -0600

    FinalCommit

---
+++ BrickBreaker/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    public GameObject player;
+    public Text txtVidas;
+    public Text txtGo;
+
+    private Vector3 velBall;
+    private int vidas;
+    private bool reStart;
+    private bool vidaRestada;
+
+	// Use this for initialization
+	void Start () {
+
+        reStart = false;
+        vidaRestada = false;
+        vidas = 3;
+
+        velBall = new Vector3(Random.Range(-6, 6f), 0f, 11f);
+        player.GetComponent<Rigidbody>().velocity = velBall;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (reStart)
+        {
+            player.transform.position = new Vector3(0f, 1f, -3f);
+            velBall = new Vector3(Random.Range(-6, 6f), 0f, 11f);
+            player.GetComponent<Rigidbody>().velocity = velBall;
+            reStart = false;
+            vidaRestada = false;
+        }
+
+        if (player.transform.position.z < -5 && !vidaRestada)
+        {
+            vidas--;
+            txtVidas.text = "Vidas: " + vidas;
+            vidaRestada = true;
+        }
+
+        if (player.transform.position.z < -20)
+        {
+            reStart = true;
+        }
+
+        if (vidas == 0)
+        {
+            txtGo.text = "GAME OVER";
+            SceneManager.LoadSceneAsync("Intro");
+        }
+
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag.Equals("Brick"))
+        {
+            velBall = new Vector3(velBall.x, 0f, velBall.z * -1);
+            player.GetComponent<Rigidbody>().velocity = velBall;
+            Destroy(other.gameObject);
+        }
+
+        else if (other.gameObject.tag.Equals("sideWalls"))
+        {
+            velBall = new Vector3(velBall.x *-1, 0f, velBall.z * -1);
+            player.GetComponent<Rigidbody>().velocity = velBall;
+        }
+
+        else
+        {
+
+            if (other.gameObject.tag.Equals("PlayerR"))
+            {
+                velBall = new Vector3(velBall.x + 0.75f, 0f, velBall.z * -1.01f);
+                player.GetComponent<Rigidbody>().velocity = velBall;
+            }
+
+            else if (other.gameObject.tag.Equals("PlayerL"))
+            {
+                velBall = new Vector3(velBall.x - 0.75f, 0f, velBall.z * -1.01f);
+                player.GetComponent<Rigidbody>().velocity = velBall;
+            }
+
+            else
+            {
+                velBall = new Vector3(velBall.x, 0f, velBall.z * -1.01f);
+                player.GetComponent<Rigidbody>().velocity = velBall;
+            }
+        }
+
+
+    }
+}
---
+++ BrickBreaker/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private Rigidbody rbPlayer;
+
+    public float forceMultiplier;
+    public float dragMultiplier;
+
+    // Use this for initialization
+    void Start () {
+        rbPlayer = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rbPlayer.AddForce(-1f * forceMultiplier, 0, 0);
+            rbPlayer.drag = 0;
+        }
+
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rbPlayer.AddForce(1f * forceMultiplier, 0, 0);
+            rbPlayer.drag = 0;
+        }
+
+        else
+            rbPlayer.drag = 1 * dragMultiplier;
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadSceneAsync("Intro");
+        }
+
+    }
+}
